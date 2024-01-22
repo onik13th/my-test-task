ed3734ce7e513f3d74fd1ff222a3a59bff2cc334
David Borun <dborun@mail.ru>
2024-01-22 20:35:27 +0300

    Обновлен файл миграции для таблицы пользователей.
    Добавлены дополнительные столбцы (возраст, пол, национальности) в таблицу пользователей.

--- migrations/20240117144803_create_users.up.sql
+++ migrations/20240117144803_create_users.up.sql
@@ -1,6 +1,12 @@
-CREATE TABLE users (
-    id bigserial not null primary key,
-    name varchar not null unique,
-    surname varchar not null unique,
-    patronymic varchar not null
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    patronymic VARCHAR(255),
+    age INTEGER,
+    gender VARCHAR(10),
+    nationalities VARCHAR(255)
 );
+--     age bigint not null,
+--     gender varchar not null,
+--     nationalities varchar not null
