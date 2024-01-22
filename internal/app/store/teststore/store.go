d29427495cc25f46af0cc3937e088ab5740f142a
David Borun <dborun@mail.ru>
2024-01-22 20:14:21 +0300

    Реализовано локальное хранилище пользователей в рамках приложения для быстрых тестов

---
+++ internal/app/store/teststore/store.go
@@ -0,0 +1,27 @@
+package teststore
+
+import (
+	"github.com/onik13th/my-test-task/internal/app/model"
+	"github.com/onik13th/my-test-task/internal/app/store"
+)
+
+type Store struct {
+	userRepository *UserRepository
+}
+
+func New() *Store {
+	return &Store{}
+}
+
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+		users: make(map[int]*model.User),
+	}
+
+	return s.userRepository
+}
